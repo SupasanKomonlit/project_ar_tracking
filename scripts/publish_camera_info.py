ff353f409a42312f80b25268d3804f1b35aac9bd
Supasan Komonlit <supasan_224@hotmail.com>
2019-06-06 10:21:12 +0700

    Finihs testing read value and publish

--- scripts/publish_camera_info.py
+++ scripts/publish_camera_info.py
@@ -16,22 +16,21 @@ import rospy
 
 from yaml import load
 
-from sensor_msgs import CameraInfo
+from sensor_msgs.msg import CameraInfo
 
 class PublishCameraInfo:
 
     def __init__( self ):
 
+        rospy.init_node( "pub_camera_info" , anonymous=False )
+
         self.name = rospy.get_name()
-        if( rospy.has_param( "camera_node_name" ) ):
-            self.name = rospy.get_param( "camera_node_name" )
+        self.camera_name = rospy.get_param( self.name + "/camera_name" , "camera")
 
 
-        self.full_path_yaml = ""
-        if( rospy.has_param('yaml') ):
-            self.full_path_yaml = rospy.get_param('yaml')
+        self.full_path_yaml = rospy.get_param( self.name + "/yaml" , "" )
 
-        self.message = CameraInfo
+        self.message = CameraInfo()
 
         topic_publish = '/' + self.name + '/camera_info'
         self.publisher = rospy.Publisher( topic_publish , CameraInfo , queue_size = 10 )
@@ -45,14 +44,14 @@ class PublishCameraInfo:
         yaml_data = load( yaml_file )
         yaml_file.close()
 
-        self.message.header.frame_id = self.name
+        self.message.header.frame_id = self.camera_name
         self.message.height = yaml_data[ 'image_height' ]
         self.message.width = yaml_data[ 'image_width' ]
         self.message.distortion_model = yaml_data[ 'distortion_model' ]
         self.message.D = yaml_data[ 'distortion_coefficients' ][ 'data' ]
         self.message.K = yaml_data[ 'camera_matrix' ][ 'data' ]
         self.message.R = yaml_data[ 'rectification_matrix' ][ 'data' ]
-        self.message.P = yaml_data[ 'distortion_model' ][ 'data' ]
+        self.message.P = yaml_data[ 'projection_matrix' ][ 'data' ]
 
 # Below part is 0 because don't have this part in basic calibration of ROS modle
         self.message.binning_x = 0
@@ -61,19 +60,17 @@ class PublishCameraInfo:
         self.message.roi.y_offset = 0
         self.message.roi.height = 0
         self.message.roi.width = 0
-        self.message.roi.dorectify = False
+        self.message.roi.do_rectify = False
 
     def spin_publish( self ):
         while( not rospy.is_shutdown() ):
             rospy.sleep( 0.1 )
-            self.message.header.stamp = rospy.get_time()
+            self.message.header.stamp = rospy.get_rostime()
 
-            self.publisher=publish( self.message )
+            self.publisher.publish( self.message )
 
 if __name__=="__main__":
     pub_camera_info = PublishCameraInfo()
-
-    rospy.init_node( "pub_camera_info" , anonymous=True )
 
     pub_camera_info.load_data()
 
